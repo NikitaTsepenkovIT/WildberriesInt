3adc560ffeab69eec18f75f186d2ee49e6583723
nikitait <nikitatsepenkovit@gmail.com>
2021-03-23 20:52:25 +0500

    1

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+
+const openModal = function () {
+	modalCart.classList.add('show');
+};
+
+const closeModal = function () {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+
+modalClose.addEventListener('click', closeModal);
+
+modalCart.addEventListener('click', (e) -> {
+	if (e.target.classList.contains('modal-close') || !e.target.closest('.modal')) {
+		closeModal();
+	}
+});
